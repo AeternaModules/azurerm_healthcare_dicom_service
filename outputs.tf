491fc001ef8701f99177f3513c872a3e34b67fd6
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:57:28 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "healthcare_dicom_services" {
+  description = "All healthcare_dicom_service resources"
+  value       = azurerm_healthcare_dicom_service.healthcare_dicom_services
+}
+
---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "healthcare_dicom_services" {
+  description = <<EOT
+Map of healthcare_dicom_services, attributes below
+Required:
+    - location
+    - name
+    - workspace_id
+Optional:
+    - data_partitions_enabled
+    - encryption_key_url
+    - public_network_access_enabled
+    - tags
+    - cors (block):
+        - allow_credentials (optional)
+        - allowed_headers (optional)
+        - allowed_methods (optional)
+        - allowed_origins (optional)
+        - max_age_in_seconds (optional)
+    - identity (block):
+        - identity_ids (optional)
+        - type (required)
+    - storage (block):
+        - file_system_name (required)
+        - storage_account_id (required)
+EOT
+
+  type = map(object({
+    location                      = string
+    name                          = string
+    workspace_id                  = string
+    data_partitions_enabled       = optional(bool, false)
+    encryption_key_url            = optional(string)
+    public_network_access_enabled = optional(bool, true)
+    tags                          = optional(map(string))
+    cors = optional(object({
+      allow_credentials  = optional(bool, false)
+      allowed_headers    = optional(list(string))
+      allowed_methods    = optional(list(string))
+      allowed_origins    = optional(list(string))
+      max_age_in_seconds = optional(number)
+    }))
+    identity = optional(object({
+      identity_ids = optional(set(string))
+      type         = string
+    }))
+    storage = optional(object({
+      file_system_name   = string
+      storage_account_id = string
+    }))
+  }))
+}
+
